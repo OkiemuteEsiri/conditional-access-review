03dbbc56773b6ff99b3914847da9b680dc73a29c
OkiemuteEsiri <okiemuteesiri37@gmail.com>
2026-09-09 04:54:50 +0100

    Fix dynamic module registration in Conditional Access tests

--- tests/test_policy_review.py
+++ tests/test_policy_review.py
@@ -1,10 +1,12 @@
 import importlib.util
 import pathlib
+import sys
 import unittest
 
 MODULE = pathlib.Path(__file__).parents[1] / "src" / "policy_review.py"
 spec = importlib.util.spec_from_file_location("policy_review", MODULE)
 policy_review = importlib.util.module_from_spec(spec)
+sys.modules[spec.name] = policy_review
 spec.loader.exec_module(policy_review)
 
 SECURE=[
